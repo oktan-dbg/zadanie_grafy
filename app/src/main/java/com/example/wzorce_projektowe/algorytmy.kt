1d7891bed28e5fcae10c34b256feb9e2c38a88bb
Oktan <107392424+octan-nautil@users.noreply.github.com>
2022-11-06 18:29:17 +0100

    dodanie algorytmu dijkstra

--- app/src/main/java/com/example/wzorce_projektowe/algorytmy.kt
+++ app/src/main/java/com/example/wzorce_projektowe/algorytmy.kt
@@ -1,4 +1,9 @@
 package com.example.wzorce_projektowe
 
-class algorytmy {
+data class Path(val nodes: List<Int>,val cost: Int)
+
+abstract class Algorithm(val graph: graph) {
+
+    abstract fun pathfind(s: Int, e:Int):Path?
+
 }
--- app/src/main/java/com/example/wzorce_projektowe/dijkstra.kt
+++ app/src/main/java/com/example/wzorce_projektowe/dijkstra.kt
@@ -1,4 +1,49 @@
 package com.example.wzorce_projektowe
 
-class dijkstra {
+import java.util.PriorityQueue
+
+data class Odleglosc(val n: Int,val c:Int)
+
+class Algorytm_dijkstra(graf:graph) : Algorithm(graf) {
+
+    override fun pathfind(s: Int, e: Int):Path? {
+
+        // poprzedni wierzchołek
+        val pooprzedni_wierzcholek = MutableList(graph.nodeCount) { -1 }
+        // Minimalna odległość
+        val odleglosc = MutableList(graph.nodeCount) { -1 }
+        // dystans z którego zaczynamy
+        odleglosc[s] = 0;
+
+        val kolejka = PriorityQueue<Odleglosc>(compareBy{it.c})
+
+        kolejka.add(Odleglosc(s,0))
+
+        while (!kolejka.isEmpty()) {
+            val obecny_wierzcholek = kolejka.remove()
+
+            for ( vertice in graph.adjacencyList[obecny_wierzcholek.n] ){
+                if(obecny_wierzcholek.c + vertice.c > odleglosc[vertice.n] && odleglosc[vertice.n] != -1) continue
+
+                odleglosc[vertice.n] = obecny_wierzcholek.c + vertice.c
+                pooprzedni_wierzcholek[vertice.n] = obecny_wierzcholek.n
+                kolejka.add(Odleglosc(vertice.n,odleglosc[vertice.n]))
+
+            }
+
+        }
+
+        if(pooprzedni_wierzcholek[e] == -1) return null
+
+        //wyliczanie ścieżki
+        var sciezka: MutableList<Int> = mutableListOf()
+        var i = e
+        do{
+            sciezka.add(i)
+            i = pooprzedni_wierzcholek[i]
+        }while(i != s)
+        return Path(sciezka.reversed(),odleglosc[e])
+
+    }
+
 }
